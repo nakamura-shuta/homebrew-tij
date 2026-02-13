0218234514018578e91d962e41bb0963e36a8efe
nakamura.shuta <nakamura.shuta@classmethod.jp>
2026-02-13 10:03:08 +0900

    Add tij formula v0.3.22

---
+++ Formula/tij.rb
@@ -0,0 +1,18 @@
+class Tij < Formula
+  desc "Text-mode Interface for Jujutsu version control system"
+  homepage "https://github.com/nakamura-shuta/tij"
+  url "https://github.com/nakamura-shuta/tij/archive/refs/tags/v0.3.22.tar.gz"
+  sha256 "24ea3fc0c8d97d2e5a66fe420f3354804ca7ede4f19f34bc1b8d5e0957f1b901"
+  license "MIT"
+
+  depends_on "rust" => :build
+  depends_on "jj"
+
+  def install
+    system "cargo", "install", "--locked", *std_cargo_args
+  end
+
+  test do
+    assert_match "tij", shell_output("#{bin}/tij --version")
+  end
+end
